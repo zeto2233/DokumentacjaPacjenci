14e9074c47dd740de4daf26ed5e4d87f97c5f31b
justynak2 <justynak88@poczta.onet.pl>
2014-06-17 11:09:55 +0200

    start

---
+++ DokumentyPacjenci/DokumentyPacjenci/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DokumentyPacjenci
+{
+    public class Class1
+    {
+        void a() { }
+        void b = 5;
+    }
+}
